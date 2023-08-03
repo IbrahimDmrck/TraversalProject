6a405ca6013bccd8f7589a9946f0fec217db5106
ibrahimdmrck@gmail.com <ibrahimdmrck@gmail.com>
2023-08-03 17:25:12 +0300

    Business katmanında service ve managerlerın yazılması

--- BusinessLayer/Abstract/IAboutService.cs
+++ BusinessLayer/Abstract/IAboutService.cs
@@ -5,9 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccessLayer.Abstract
+namespace BusinessLayer.Abstract
 {
-    public interface IGuideDal:IGenericDal<Guide>
+    public interface IAboutService:IGenericService<About>
     {
     }
+
 }
