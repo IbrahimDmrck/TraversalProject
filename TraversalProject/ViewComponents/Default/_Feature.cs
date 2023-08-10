7899114ad1da090c754a9bba889bfb392281c6d9
ibrahimdmrck@gmail.com <ibrahimdmrck@gmail.com>
2023-08-10 18:12:24 +0300

    Ana sayfanın parçalanması ve veri tabanından verilerin çekilmeye başlanması

---
+++ TraversalProject/ViewComponents/Default/_Feature.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalProject.ViewComponents.Default
+{
+    public class _Feature:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+            var value = featureManager.TGetList();
+            return View();
+        }
+    }
+}
