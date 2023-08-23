7290c1eb14c0bcb793a47f8a254a5620c1197334
ibrahimdmrck@gmail.com <ibrahimdmrck@gmail.com>
2023-08-23 18:18:44 +0300

    yorum eklem işlemi için viewcomponen denedik ama olmadı onun yerine ajax post deneyeceğim

---
+++ TraversalProject/Controllers/CommentController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalProject.Controllers
+{
+    public class CommentController : Controller
+    {
+        CommentManager commentManager = new CommentManager(new EfCommentDal());
+
+        [HttpGet]
+        public PartialViewResult AddComment()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult AddComment(Comment comment)
+        {
+            comment.CommentDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            comment.CommentState = true;
+            commentManager.TAdd(comment);
+            return PartialView();
+        }
+    }
+}
