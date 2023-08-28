93259dc50589196e2eb22a63a7b8ed2faaa5906a
ibrahimdmrck@gmail.com <ibrahimdmrck@gmail.com>
2023-08-28 21:49:44 +0300

    kayıt olma işlmeinde Identity ye göre şifre işlemi yapıldı , şifre doğrulamaları türkçeleştirildi

---
+++ TraversalProject/Models/CustomIdentityValidator.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalProject.Models
+{
+    public class CustomIdentityValidator : IdentityErrorDescriber
+    {
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordTooShort",
+                Description = $"Parola Minimum {length} Karakter Olmalıdır."
+            };
+        }
+
+        public override IdentityError PasswordRequiresUpper()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresUpper",
+                Description = "Parola Minimum 1 Büyük Harf İçermelidir."
+            };
+        }
+        public override IdentityError PasswordRequiresDigit()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresDigit",
+                Description = "Parola Minimum 1 Rakam İçermelidir."
+            };
+        }
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresLower",
+                Description = "Parola Minimum 1 Küçük Harf İçermelidir."
+            };
+        }
+
+        public override IdentityError PasswordRequiresNonAlphanumeric()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresNonAlphanumeric",
+                Description = "Parola Minimum 1 Sembol İçermelidir ['!#+&/()=?/.,-|}[]@:;<>*]"
+            };
+        }
+
+    }
+}
