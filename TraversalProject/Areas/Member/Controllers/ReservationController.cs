830cf8866a678c8153c60f3d1b31bdbe7984c5a7
ibrahimdmrck@gmail.com <ibrahimdmrck@gmail.com>
2023-09-12 21:59:31 +0300

    Kullaııcı rezervasyon kaydı talebi oluşturma yapıldı

--- TraversalProject/Areas/Member/Controllers/ReservationController.cs
+++ TraversalProject/Areas/Member/Controllers/ReservationController.cs
@@ -1,5 +1,8 @@
-﻿using EntityLayer.Concrete;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +11,11 @@ using System.Threading.Tasks;
 namespace TraversalProject.Areas.Member.Controllers
 {
     [Area("Member")]
+    [Route("Member/[controller]/[action]")]
     public class ReservationController : Controller
     {
+        DestinationManager _destinationManager = new DestinationManager(new EfDestinationDal());
+        ReservationManager _reservationManager = new ReservationManager(new EfReservationDal());
         public IActionResult MyCurrentReservation()
         {
             return View();
@@ -23,13 +29,22 @@ namespace TraversalProject.Areas.Member.Controllers
         [HttpGet]
         public IActionResult NewReservation()
         {
+            List<SelectListItem> values = (from x in _destinationManager.TGetList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.City,
+                                               Value = x.DestinationID.ToString()
+                                           }).ToList();
+            ViewBag.v = values;
             return View();
         }
 
         [HttpPost]
         public IActionResult NewReservation(Reservation p)
         {
-            return View();
+            p.AppUserId = 1;
+            _reservationManager.TAdd(p);
+            return RedirectToAction("NewReservation", "Reservation");
 
         }
     }
