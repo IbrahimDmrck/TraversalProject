be105eff92cb3dd1a06e00aa23082ddc8d0fe4c5
ibrahimdmrck@gmail.com <ibrahimdmrck@gmail.com>
2023-09-13 22:33:06 +0300

    rezervasyonlar ve rotaların ilişkilendirilmesi

--- DataAccessLayer/EntityFramework/EfReservationDal.cs
+++ DataAccessLayer/EntityFramework/EfReservationDal.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.Repository;
 using EntityLayer.Concrete;
 using System;
@@ -9,7 +10,25 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.EntityFramework
 {
-   public class EfReservationDal : GenericRepository<Reservation>, IReservationDal
+    public class EfReservationDal : GenericRepository<Reservation>, IReservationDal
     {
+        public List<Reservation> GetListWithReservationByAccepted(int id)
+        {
+            throw new NotImplementedException();
+            //using (var context=new Context())
+            //{
+
+            //}
+        }
+
+        public List<Reservation> GetListWithReservationByPrevious(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Reservation> GetListWithReservationByWaitApproval(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
