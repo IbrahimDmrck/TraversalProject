d14916b53c0d731e76d05ff21ffffe02fa3a66ec
ibrahimdmrck@gmail.com <ibrahimdmrck@gmail.com>
2023-09-19 22:30:31 +0300

    Admin areas ın oluşturulması ve admin UI ın dağil edilmesi yapıldı

--- TraversalProject/Startup.cs
+++ TraversalProject/Startup.cs
@@ -78,6 +78,15 @@ namespace TraversalProject
                   pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
                 );
             });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+
         }
     }
 }
