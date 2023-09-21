b582fba36e971077ff4ac610d6ef15f400df2f0c
ibrahimdmrck@gmail.com <ibrahimdmrck@gmail.com>
2023-09-21 22:17:08 +0300

    Admin dashboard controller oluşturuldu , dashboard sayfası ayarlandı ve viewComponentler oluşturuldu

---
+++ TraversalProject/ViewComponents/AdminDashboard/_CardsStatistic.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalProject.ViewComponents.AdminDashboard
+{
+    public class _CardsStatistic : ViewComponent
+    {
+        Context context = new Context();
+        public IViewComponentResult Invoke(int id)
+        {
+            ViewBag.Tours = context.Destinations.Count();
+            ViewBag.Users = context.Users.Count();
+            return View();
+        }
+    }
+}
---
+++ TraversalProject/ViewComponents/AdminDashboard/_DashboardBanner.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalProject.ViewComponents.AdminDashboard
+{
+    public class _DashboardBanner:ViewComponent
+    {
+        public IViewComponentResult Invoke(int id)
+        {
+            
+            return View();
+        }
+    }
+}
