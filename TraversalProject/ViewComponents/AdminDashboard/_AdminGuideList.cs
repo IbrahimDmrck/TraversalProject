64131703d604b4becc744f9759b3a434bdfc7622
ibrahimdmrck@gmail.com <ibrahimdmrck@gmail.com>
2023-09-25 21:38:31 +0300

    Admin dashboard sayfası viewComponentlere ayrıldı

---
+++ TraversalProject/ViewComponents/AdminDashboard/_AdminGuideList.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalProject.ViewComponents.AdminDashboard
+{
+    public class _AdminGuideList : ViewComponent
+    {
+        Context context = new Context();
+        public IViewComponentResult Invoke(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ TraversalProject/ViewComponents/AdminDashboard/_TotalRevenue.cs
@@ -0,0 +1,19 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalProject.ViewComponents.AdminDashboard
+{
+    public class _TotalRevenue : ViewComponent
+    {
+        Context context = new Context();
+        public IViewComponentResult Invoke(int id)
+        {
+            
+            return View();
+        }
+    }
+}
