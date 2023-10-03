e277f729840885c31e4d87a310ee9e0c87a71c3c
ibrahimdmrck@gmail.com <ibrahimdmrck@gmail.com>
2023-10-03 22:17:57 +0300

    Mail gönderme işlemi yapıldı

---
+++ TraversalProject/Areas/Admin/Controllers/MailController.cs
@@ -0,0 +1,44 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TraversalProject.Models;
+
+namespace TraversalProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(MailRequest mailRequest)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("Admin", "dmrckibo@gmail.com");
+            mimeMessage.From.Add(mailboxAddressFrom);
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User",mailRequest.RecieverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = mailRequest.Body;
+
+            mimeMessage.Subject = mailRequest.Subject;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com",587,false);
+            client.Authenticate("dmrckibo@gmail.com", "qacbskcqsvdmmhjk");
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+            return View();
+        }
+    }
+}
